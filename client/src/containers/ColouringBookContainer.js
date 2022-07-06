89f2364ebe33d49935776b25e66048c24e0a9d23
Jennifer Lane <jennifer.lane610@gmail.com>
2022-07-06 17:03:34 +0100

    Palette created, Mandala SVG can be coloured in and routing to ColouringBookContainer from App.js works.

---
+++ client/src/containers/ColouringBookContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Mandala from '../components/ColouringBook/Mandala';
+
+const ColouringBookContainer = () => {
+    return (
+        <div>
+            <h2>Colour Away</h2>
+            <Mandala />
+        </div>
+    )
+}
+
+export default ColouringBookContainer;
