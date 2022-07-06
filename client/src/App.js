43992b6486fe3e22806254dfee90b140151d084c
Fiona <fiona_berkery@hotmail.com>
2022-07-06 11:18:08 +0100

    react router set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,41 @@
 
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
 
     <>
-      <h2> Hello everyone </h2>
+      
+      <Router>
+      <NavBar/>
+      <Switch> 
+
+        <Route exact path="/">
+          {/* home component */}
+        </Route>
+
+        <Route path="/breathe">
+          {/* breathing component */}
+        </Route>
+
+        <Route path="/colour">
+          {/* colouring component */}
+        </Route>
+
+        <Route path="/journal">
+          {/* journal component */}
+        </Route>
+
+        <Route path="/listen">
+          {/* sounds component */}
+        </Route>
+
+      </Switch>
+      </Router>
+
     </>
   );
 }
