08b4639f405c8d6506343b773b9cf63bba076145
ElinVS <elinvsvennberg@gmail.com>
2022-07-06 13:09:47 +0100

    breathe component started

---
+++ client/src/components/Breathe.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+const Breathe = () => {
+
+    return(
+        <>
+
+        <div className="circle-container">
+
+            <div className="circle"> </div>
+
+                <div className="pointer-container">
+                    <div className="pointer"></div>
+                </div>
+
+            <div className="gradient-circle"></div>
+       
+        </div>
+        
+        </>
+    )
+
+}
+
+
+export default Breathe
+
+
+
+//4 divs
