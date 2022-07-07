320423ee4dc37f03c372ae287f7387eac883e4d7
Jennifer Lane <jennifer.lane610@gmail.com>
2022-07-07 16:20:57 +0100

    Added MandalaImage2, colouring in paths work, some very teeny tiny paths hardcoded to black to blend in with the line work because they're just too tiny to even notice they're there.

--- client/src/containers/ColouringBookContainer.js
+++ client/src/containers/ColouringBookContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import MandalaImage1 from '../components/ColouringBook/MandalaImage1';
+import MandalaImage2 from '../components/ColouringBook/MandalaImage2';
 import VillageStairsImage from '../components/ColouringBook/VillageStairsImage';
 
 const ColouringBookContainer = () => {
     return (
         <div>
             <h2>Colour Away</h2>
+            <MandalaImage2 />
             <VillageStairsImage />
             <MandalaImage1 />
         </div>
