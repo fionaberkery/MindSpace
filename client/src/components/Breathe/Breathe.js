c69eb74846e5346e62c92fdb1352427b96ef180a
ElinVS <elinvsvennberg@gmail.com>
2022-07-10 21:33:35 +0100

    toggle added to breathe to change themes

--- client/src/components/Breathe/Breathe.js
+++ client/src/components/Breathe/Breathe.js
@@ -1,46 +1,52 @@
-import React,{useState, useEffect, useRef} from "react";
+import React,{useState, createContext} from "react";
+import ReactSwitch from "react-switch";
 import './breathe.css'
 
-
+export const ThemeContext = createContext(null)
 
 const Breathe = () => {
 
-
     const [currentWord, setCurrentWord]= useState(true)
-
 
     setTimeout (()=>{
         setCurrentWord (!currentWord);
-    }, 9200)
+    }, 7000)
 
 
+    const [theme,setTheme] = useState('light')
+
+    const toggleTheme = () =>{
+        setTheme((current) => (current === 'light' ? 'dark' : 'light'))
+    }
 
     return(
         <>
-    
-        <section className="breathe-container">
 
-            <div className="circle-container">
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+            <section className="breathe-container" id={theme}>
+
+            <label> {theme === 'light' ? "Breathe in the sky" : "Breathe in the Forrest"} </label>
+            <ReactSwitch onChange={toggleTheme} checked={theme === "light"}/>
+            
+                <div className="circle-container">
         
-                <div className="circle"> 
-                    <div className="word-container">
-                       { currentWord ? <p className="word">Breathe In </p> : <p className="word">Breathe Out </p> }
+                    <div className="circle"> 
+                        <div className="word-container">
+                            { currentWord ? <p className="word"> Breathe In </p> : <p className="word"> Breathe Out </p> }
+                        </div>
                     </div>
-                </div>
 
-                <div className="pointer-container">
-                    <div className="pointer"></div>
-                </div>
+                    <div className="pointer-container">
+                        <div className="pointer"></div>
+                    </div>
                 
-                <div className="gradient-circle"></div>
-            </div>
-        </section>
+                    <div className="gradient-circle"></div>
+                </div>
+            </section>
+        </ThemeContext.Provider>
+
         </>
     )
 }
 
 export default Breathe
-
-
-
-//4 divs
