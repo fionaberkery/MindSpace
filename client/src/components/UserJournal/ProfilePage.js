a0b2c892e8893b6ca27d792661fcedb219088e15
Fiona <fiona_berkery@hotmail.com>
2022-07-11 17:16:36 +0100

    more css

--- client/src/components/UserJournal/ProfilePage.js
+++ client/src/components/UserJournal/ProfilePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {motion} from 'framer-motion'
 import './carousel.css'
+import './userProfile.css'
 import { Link } from "react-router-dom";
 
 const ProfilePage = ({currentUserJournalEntries}) => {
@@ -13,7 +14,7 @@ const ProfilePage = ({currentUserJournalEntries}) => {
         <div className="sub-profile-page">
 
         <div className="profile-page-header">
-            <h1>  Profile </h1> 
+            <h1 className="my-journal-title">  My Journal </h1> 
             <button> <Link to="/home"> Log out </Link> </button>
             <button> <Link to="/create"> New journal entry </Link></button>     
         </div>
