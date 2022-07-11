02a2e0a699fb2fa68ac6698c6c4236aa1ed5011c
Fiona <fiona_berkery@hotmail.com>
2022-07-11 10:54:23 +0100

    fixed paths for iages

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { MouseParallaxChild, MouseParallaxContainer } from "react-parallax-mouse";
-import journalImage from "/Users/fionaberkery/CodeClan_work/FinalProject/Wellbeing_FirstAidKit/client/src/static/home/journal.png"
-import './home.css'
+
 
 const Home = () => {
 
