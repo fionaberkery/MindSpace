babf85c1dcb98be051058afd7ef97065995f78e5
ElinVS <elinvsvennberg@gmail.com>
2022-07-12 11:28:35 +0100

    colouring css

--- client/src/components/ColouringBook/DefaultPage.js
+++ client/src/components/ColouringBook/DefaultPage.js
@@ -2,6 +2,8 @@ import React from 'react'
 import village from '../../static/colouring/VillageStairsImage.png';
 import mandala1 from '../../static/colouring/Mandala1Image.png';
 import mandala2 from '../../static/colouring/Mandala2Image.png';
+import '../../static/colouring/defaultpage.css'
+import "../../static/colouring/colouring.css";
 
 
 
